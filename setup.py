738b4754ff9725aff6904e1be069468a3b0928f7
Ishuman Agarwal <mann.agrawal17@gmail.com>
2024-01-23 19:27:31 +0530

    Add dependencies and setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "CellSegmentation",
+    version = "0.0.1",
+    author="Ishuman Agarwal",
+    author_email="mann.agrawal17@gmail.com",
+    packages= find_packages(),
+    install_requires = []       
+)
