7a23f8b0236e84a734325afb873d2c0ffa9253ed
Ishuman Agarwal <mann.agrawal17@gmail.com>
2024-02-19 15:42:21 +0530

    added training notebook and artifact dir path.

---
+++ CellSegmentation/constant/training_pipeline/__init__.py
@@ -0,0 +1,8 @@
+ARTIFACTS_DIR: str = "artifacts"
+
+"""Data Ingestion related constant start with DATA_INGESTION var name"""
+
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str= "feature_store"
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/uc?/export=download&id=17vDLJrk4iYtysocirKNCDuDIOAsX0-17"
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Cell-Segmentation
+
+# workflow
+> 1. Constants
+> 2. Entity
+> 3. Components
+> 4. Pipeline
+> 5. App.py
