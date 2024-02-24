8bfc0cff3a289736a2bbd9f90cde4ce970ea62eb
Ishuman Agarwal <mann.agrawal17@gmail.com>
2024-02-24 19:40:46 +0530

    Add data validation functionality

--- CellSegmentation/constant/training_pipeline/__init__.py
+++ CellSegmentation/constant/training_pipeline/__init__.py
@@ -4,4 +4,15 @@ ARTIFACTS_DIR: str = "artifacts"
 
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str= "feature_store"
-DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/11Htkp1f3ABjz-KYafMpz9fajaWHTTKLt/view?usp=sharing"
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1487BQ-cmMHYSzLpVcE7toGNnfW1nhP7d/view?usp=sharing"
+
+
+
+
+"""
+Data Validation Related Content Start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+DATA_VALIDATION_ALL_REQUIRED_FILES = ['train','valid', 'test', 'data.yaml']
